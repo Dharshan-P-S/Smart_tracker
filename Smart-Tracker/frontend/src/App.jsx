cbd485b2637a9241f0623b9c6651ffa421e0be7f
Dharshan <dharshan280706@gmail.com>
2025-05-18 15:24:25 +0530

    feat: Update ToastContainer position from top-center to top-right

--- Smart-Tracker/frontend/src/App.jsx
+++ Smart-Tracker/frontend/src/App.jsx
@@ -82,7 +82,7 @@ function App() {
       </Routes>
 
       <ToastContainer
-          position="top-center"
+          position="top-right"
           autoClose={3000}
           hideProgressBar={false}
           newestOnTop={false}
